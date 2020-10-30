fc32b75fa8da13ebed7dbafdd36873cb196e5889
Tada, Tadashi <t@tdtds.jp>
2020-10-30 08:41:09 +0000

    fix

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ for f in .*; do
 	if `echo ${ignore[@]} | grep -q "$f"`; then continue; fi
 	test ! -L ~/$f && ln -s $PWD/$f ~/
 done
-which git >/dev/null && config --global include.path '~/.gitconfig.local'
+which git >/dev/null && git config --global include.path '~/.gitconfig.local'
