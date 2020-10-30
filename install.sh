bd2e190fecef992d0fae9585b183253c0f88575e
Tada, Tadashi <t@tdtds.jp>
2020-10-30 08:35:30 +0000

    use include to git config local settings

--- install.sh
+++ install.sh
@@ -6,7 +6,4 @@ for f in .*; do
 	if `echo ${ignore[@]} | grep -q "$f"`; then continue; fi
 	test ! -L ~/$f && ln -s $PWD/$f ~/
 done
-
-# add some value into gitconfig
-git config --global user.name "Tada, Tadashi"
-git config --global user.email t@tdtds.jp
+which git >/dev/null && config --global include.path '~/.gitconfig.local'
